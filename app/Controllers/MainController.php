78b313089a297b72cbe57ddf438f9b3ce43bd031
AnneLineC <anneline.col@gmail.com>
2021-09-10 11:56:50 +0200

    Mise en place de la route, du MainController et de la vue home

---
+++ app/Controllers/MainController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Pokedex\Controllers;
+
+class MainController {
+
+    function home() {
+        $this->show('home');
+    }
+
+    /**
+     * Méthode show() permettant d'afficher le bon template -> private car doit être appelée uniquement au sein du controller
+     */
+    private function show($viewName, $viewData = []) {
+
+        // On rend la variable $router, définie sur index.php, accessible depuis la méthode show() et donc depuis toutes les vues, afin de pouvoir générer les liens automatiquement avec altorouter
+        global $router;
+
+        require __DIR__ . '/../views/header.part.php';
+        // on inclut une vue selon la valeur du paramètre $viewName
+        require __DIR__ . '/../views/' . $viewName . '.tpl.php';
+        require __DIR__ . '/../views/footer.part.php';
+
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,1 +1,51 @@
+<?php
+
+// Pour utiliser les librairies gérées par Composer (ici Altorouteur)
+require __DIR__ . '/../vendor/autoload.php';
+
+// Instanciation d'Altorouteur
+$router = new AltoRouter();
+
+// On déclare à Altorouteur que notre site est placé dans un sous-répertoire
+// BASE_URI est défini dans le .htaccess
+$router->setBasePath($_SERVER['BASE_URI']);
+
+
+//* Tableau des routes
+
+$routes = [
+    // Route 'home'
+    [
+        'GET',
+        '/',
+        [
+            'controller' => 'MainController',
+            'action' => 'home'
+        ],
+        'home'
+    ],
+];
+
+//* Transmission des routes à Altorouteur
+
+$router->addRoutes($routes);
+
+
+//* Dispatcher, qui va rediriger vers la bonne action en fonction de l'URL
+
+$match = $router->match();
+
+if ($match !== false) {
+    // On récupére avec altorouteur (le résultat de match()) les données de la route (mappé plus haut)
+    $controllerToUse = 'Pokedex\\Controllers\\' . $match['target']['controller'];
+    $methodToUse = $match['target']['action'];
+
+    // On instancie le controller et exécute la méthode
+    $controller = new $controllerToUse();
+    $controller->$methodToUse();
+}
+else {
+    // Si l'URL n'existe pas dans notre tableau de routes, on renvoie une erreur 404
+    http_response_code('404');
+}
 
